74aa521129f926a6829cbb4754e6d21fbcf113b4
AndreaCavicchioli <andrea.17.cavicchioli@gmail.com>
2018-11-12 09:59:08 +0100

    Creazione Interfaccia Grafica

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -20,50 +20,14 @@ namespace WindowsFormsApplication1
         public Form1()
         {
             
-            InitializeComponent();
-          
-            //
-            // In a using statement, acquire the SqlConnection as a resource.
-            //
-            con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\studente\\Documents\\visual studio 2015\\Projects\\WindowsFormsApplication1\\WindowsFormsApplication1\\Database1.mdf\";Integrated Security=True;");
-            con.Open();
+  
         }
 
-        private void button_fine_Click(object sender, EventArgs e)
-        {
-            con.Close();
-            Close();
-        }
+       
 
-        private void button_insert_Click(object sender, EventArgs e)
-        {
-            SqlCommand myCommand = new SqlCommand("INSERT INTO Anagrafica (Id, cognome, nome) " +
-                                     "Values (1, 'Braccini', 'Gianluca')", con);
+    
 
-            myCommand.ExecuteNonQuery();
+       
 
-        }
-
-        private void button_read_Click(object sender, EventArgs e)
-        {
-           try
-          {
-                SqlDataReader myReader = null;
-                SqlCommand myCommand = new SqlCommand("select * from Anagrafica",
-                                                         con);
-                myReader = myCommand.ExecuteReader();
-                
-                DataTable dt = new DataTable();
-                dt.Load(myReader);
-                dataGridView1.AutoGenerateColumns = true;
-                dataGridView1.DataSource = dt;
-                dataGridView1.Refresh();
-               
-            }
-            catch (System.IO.IOException errore)
-            {
-               Console.WriteLine(errore.ToString());
-            }
-        }
     }
 }
