7b5c6cd8bd02fefafb8e23f4c75b30a3361aaef6
Lorenzo_Aldobrandi <aldobrandi.lorenzo@itismeucci.com>
2018-11-12 09:17:59 +0100

    Primo commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_fine = new System.Windows.Forms.Button();
+            this.button_insert = new System.Windows.Forms.Button();
+            this.button_read = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_fine
+            // 
+            this.button_fine.Location = new System.Drawing.Point(197, 195);
+            this.button_fine.Name = "button_fine";
+            this.button_fine.Size = new System.Drawing.Size(75, 23);
+            this.button_fine.TabIndex = 0;
+            this.button_fine.Text = "Esci";
+            this.button_fine.UseVisualStyleBackColor = true;
+            this.button_fine.Click += new System.EventHandler(this.button_fine_Click);
+            // 
+            // button_insert
+            // 
+            this.button_insert.Location = new System.Drawing.Point(12, 195);
+            this.button_insert.Name = "button_insert";
+            this.button_insert.Size = new System.Drawing.Size(75, 23);
+            this.button_insert.TabIndex = 1;
+            this.button_insert.Text = "Inserisci";
+            this.button_insert.UseVisualStyleBackColor = true;
+            this.button_insert.Click += new System.EventHandler(this.button_insert_Click);
+            // 
+            // button_read
+            // 
+            this.button_read.Location = new System.Drawing.Point(102, 195);
+            this.button_read.Name = "button_read";
+            this.button_read.Size = new System.Drawing.Size(75, 23);
+            this.button_read.TabIndex = 2;
+            this.button_read.Text = "Leggi";
+            this.button_read.UseVisualStyleBackColor = true;
+            this.button_read.Click += new System.EventHandler(this.button_read_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(22, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button_read);
+            this.Controls.Add(this.button_insert);
+            this.Controls.Add(this.button_fine);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_fine;
+        private System.Windows.Forms.Button button_insert;
+        private System.Windows.Forms.Button button_read;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+        SqlConnection con;
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+          
+            //
+            // In a using statement, acquire the SqlConnection as a resource.
+            //
+            con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\studente\\Documents\\visual studio 2015\\Projects\\WindowsFormsApplication1\\WindowsFormsApplication1\\Database1.mdf\";Integrated Security=True;");
+            con.Open();
+        }
+
+        private void button_fine_Click(object sender, EventArgs e)
+        {
+            con.Close();
+            Close();
+        }
+
+        private void button_insert_Click(object sender, EventArgs e)
+        {
+            SqlCommand myCommand = new SqlCommand("INSERT INTO Anagrafica (Id, cognome, nome) " +
+                                     "Values (1, 'Braccini', 'Gianluca')", con);
+
+            myCommand.ExecuteNonQuery();
+
+        }
+
+        private void button_read_Click(object sender, EventArgs e)
+        {
+           try
+          {
+                SqlDataReader myReader = null;
+                SqlCommand myCommand = new SqlCommand("select * from Anagrafica",
+                                                         con);
+                myReader = myCommand.ExecuteReader();
+                
+                DataTable dt = new DataTable();
+                dt.Load(myReader);
+                dataGridView1.AutoGenerateColumns = true;
+                dataGridView1.DataSource = dt;
+                dataGridView1.Refresh();
+               
+            }
+            catch (System.IO.IOException errore)
+            {
+               Console.WriteLine(errore.ToString());
+            }
+        }
+    }
+}
